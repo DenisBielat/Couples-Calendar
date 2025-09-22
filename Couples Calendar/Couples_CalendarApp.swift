6fef4b933abdbb33c1423d71174d53cddc3b0b5e
Denis Bielat <denis.bielat@gmail.com>
2025-09-22 17:47:42 -0500

    Initial Commit

---
+++ Couples Calendar/Couples_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Couples_CalendarApp.swift
+//  Couples Calendar
+//
+//  Created by Denis Bielat on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Couples_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
